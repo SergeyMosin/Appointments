b5c3ea0fb9a06245225c9f2c4fe75ba3468d46a5
Sergey Mosin <sergey@srgdev.com>
2020-02-25 14:52:32 -0500

    Persist tokens after update

--- lib/InstallHook.php
+++ lib/InstallHook.php
@@ -21,39 +21,41 @@ class InstallHook{
 
         // TODO: check if the value already set, othervice update breaks everything ...
 
-
-
-        $config->setAppValue($AppName, 'hk',
-            bin2hex(openssl_random_pseudo_bytes(32,$is_good)));
-        $config->setAppValue($AppName, 'tiv',
-            bin2hex(openssl_random_pseudo_bytes(
-                openssl_cipher_iv_length(PageController::CIPHER),
-                $is_good)));
-
-
-
-
-        $u=$um->get($UserId);
-        try {
-            $a=$am->getAccount($um->get($UserId));
-            $ap=$a->getProperty(IAccountManager::PROPERTY_PHONE);
-            $phone=$ap->getValue();
-        } catch (PropertyDoesNotExistException $e) {
-            $phone='';
+        if(empty($config->getAppValue($AppName,'hk'))){
+            $config->setAppValue($AppName, 'hk',
+                bin2hex(openssl_random_pseudo_bytes(32, $is_good)));
+        }
+        if(empty($config->getAppValue($AppName,'tiv'))) {
+            $config->setAppValue($AppName, 'tiv',
+                bin2hex(openssl_random_pseudo_bytes(
+                    openssl_cipher_iv_length(PageController::CIPHER),
+                    $is_good)));
         }
 
-        $email=$u->getEMailAddress();
+        $u=$um->get($UserId);
 
+        if(empty($config->getUserValue($UserId,$AppName,'phone'))){
+            try {
+                $a = $am->getAccount($u);
+                $ap = $a->getProperty(IAccountManager::PROPERTY_PHONE);
+                $phone = $ap->getValue();
+            } catch (PropertyDoesNotExistException $e) {
+                $phone = '';
+            }
+            $config->setUserValue(
+                $UserId,
+                $AppName,
+                'phone',
+                $phone);
+        }
 
-        $config->setUserValue(
-            $UserId,
-            $AppName,
-            'phone',
-            empty($phone)?'':$phone);
-        $config->setUserValue(
-            $UserId,
-            $AppName,
-            'email',
-            empty($email)?'':$email);
+        if(empty($config->getUserValue($UserId,$AppName,'email'))) {
+            $email = $u->getEMailAddress();
+            $config->setUserValue(
+                $UserId,
+                $AppName,
+                'email',
+                empty($email) ? '' : $email);
+        }
     }
 }
