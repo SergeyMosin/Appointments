8b96954f040f4b576a0e3b4f84e71662eb48ac1d
Sergey Mosin <sergey@srgdev.com>
2020-02-26 19:36:43 -0500

    make sure there is a color

--- src/components/NavAccountItem.vue
+++ src/components/NavAccountItem.vue
@@ -63,11 +63,15 @@
                         l=cals.length
                         for(let i=0;i<l;i++){
                             let cal=cals[i].split(String.fromCharCode(30))
+                            let c=this.detectColor(cal[1])
+                            if(c===false){
+                                c="#8768bd"
+                            }
                             this.$set(this.calendars, i, {
                                 // "Personal", "#795AAB", "personal"
                                 icon: f_host + cal[1].slice(1),
                                 name: cal[0],
-                                clr: cal[1],
+                                clr: c,
                                 url: cal[2]
                             })
                         }
@@ -88,7 +92,29 @@
             setCalendarFromIndex:function (idx) {
                 const c=this.calendars[idx]
                 this.$emit('calSelected',c)
+            },
+
+            /**
+             * Detects a color from a given string
+             *
+             * @param {String} color The color to get the real RGB hex string from
+             * @returns {string|boolean|*} String if color detected, boolean if not
+             */
+            detectColor(color) {
+                if (/^(#)((?:[A-Fa-f0-9]{3}){1,2})$/.test(color)) { // #ff00ff and #f0f
+                    return color
+                } else if (/^((?:[A-Fa-f0-9]{3}){1,2})$/.test(color)) { // ff00ff and f0f
+                    return '#' + color
+                } else if (/^(#)((?:[A-Fa-f0-9]{8}))$/.test(color)) { // #ff00ffff and #f0ff
+                    return color.substr(0, 7)
+                } else if (/^((?:[A-Fa-f0-9]{8}))$/.test(color)) { // ff00ffff and f0ff
+                    return '#' + color.substr(0, 6)
+                }
+
+                return false
             }
+
+
         }
     }
 </script>
