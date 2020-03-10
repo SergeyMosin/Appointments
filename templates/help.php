dadc9cc0d1a6fc4447e99a2ab568558dc78b49f1
Sergey Mosin <sergey@srgdev.com>
2020-03-10 18:41:26 -0400

    typos

--- templates/help.php
+++ templates/help.php
@@ -6,9 +6,9 @@
     <h2 class="srgdev-appt-hs-h1">3. Add Appointments</h2>
     <p class="srgdev-appt-hs-p">Please use the "Add Appointments" dialog.</p>
     <p class="srgdev-appt-hs-p"><strong>Timezone Options:</strong></p>
-    <p class="srgdev-appt-hs-p"><span style="text-decoration: underline">Local (floating)</span> - this option should be used if you are booking appointments for real location, like an office or a store. "Floating" values are not bound to any time zone in particular, and will represent the same hour, minute, and second value regardless of which time zone is currently being observed.</p>
+    <p class="srgdev-appt-hs-p"><span style="text-decoration: underline">Local (floating)</span> - this option should be used if you are booking appointments for a real location, like an office or a store. "Floating" values are not bound to any time zone in particular, and will represent the same hour, minute, and second value regardless of which time zone is currently being observed.</p>
     <p class="srgdev-appt-hs-p"><span style="text-decoration: underline">Calendar Timezone</span> - your calendar's timezone will be used. This option should be used if you are booking events where people participate from different locations, like phone calls or video conferences.</p>
-    <p class="srgdev-appt-hs-p" style="font-style: italic">This options mostly effect the .ics file attached to confirmation email. Also, Gnome Calendar does not like "floating" timezones.</p>
+    <p class="srgdev-appt-hs-p" style="font-style: italic">This options mostly affect the .ics file attached to the confirmation email. Also, Gnome Calendar does not like "floating" timezones.</p>
     <h2 class="srgdev-appt-hs-h1">4. Share the Public Link</h2>
     <p class="srgdev-appt-hs-p">Enable sharing and pass along the public form link. Seven upcoming days of appointments are going to be available on the booking page.</p>
     <h2 class="srgdev-appt-hs-h1">5. Check Status in the Calendar</h2>
