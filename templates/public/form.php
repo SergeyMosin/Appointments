f7ede023182f5973ff3130e89a9845fb62c16e1c
rakekniven <mark.ziegler@rakekniven.de>
2020-04-17 19:52:13 +0200

    l10n: Changed case of words
    Signed-off-by: rakekniven <mark.ziegler@rakekniven.de>

--- templates/public/form.php
+++ templates/public/form.php
@@ -17,7 +17,7 @@ style('appointments', 'form');
         <h3 class="srgdev-ncfp-form-header">'.
             ($_['appt_state']==='2'
                 ?$_['appt_form_title']
-                :$l->t('No Appointments Available')).'</h3>'
+                :$l->t('No appointments available')).'</h3>'
     ?>  <div class="srgdev-ncfp-form-main-cont">
         <label class="srgdev-ncfp-form-label"><?php p($l->t("Date & Time:")) ?></label>
         <div id="srgdev-ncfp_sel_cont">
@@ -28,18 +28,18 @@ style('appointments', 'form');
             </select>
         </div>
         <label class="srgdev-ncfp-form-label"><?php p($l->t("Name:"))?></label>
-        <input name="name" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter Name")); ?>" id="srgdev-ncfp_fname" class="srgdev-ncfp-form-input" type="text">
+        <input name="name" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter name")); ?>" id="srgdev-ncfp_fname" class="srgdev-ncfp-form-input" type="text">
         <label class="srgdev-ncfp-form-label"><?php p($l->t("Email:"));?></label>
-        <input name="email" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter Email")); ?>" id="srgdev-ncfp_femail" class="srgdev-ncfp-form-input" type="email">
+        <input name="email" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter email")); ?>" id="srgdev-ncfp_femail" class="srgdev-ncfp-form-input" type="email">
         <label class="srgdev-ncfp-form-label"><?php p($l->t("Phone:")); ?></label>
-        <input name="phone" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter Phone Number")); ?>" id="srgdev-ncfp_fphone" class="srgdev-ncfp-form-input" type="tel"><?php
+        <input name="phone" <?php echo $disabled ?>placeholder="<?php p($l->t("Enter phone number")); ?>" id="srgdev-ncfp_fphone" class="srgdev-ncfp-form-input" type="tel"><?php
         if(!empty($_['appt_gdpr'])){
             echo '<div class="srgdev-ncfp-chb-cont"><input class="checkbox" type="checkbox" id="appt_gdpr_id"/>'.(strpos($_['appt_gdpr'],'appt_gdpr_id')===false?'<label for="appt_gdpr_id">'.$_['appt_gdpr'].'</label>':$_['appt_gdpr']).'</div>';
         }
         ?>
         <button id="srgdev-ncfp_fbtn" <?php echo $disabled ?>class="primary srgdev-ncfp-form-btn"><?php
             // TRANSLATORS This is the text for the "Book Now" button, on the appointment form.
-            p($l->t("Book Now"));
+            p($l->t("Book now"));
             ?></button>
     </div>
     </form>
