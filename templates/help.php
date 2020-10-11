c6811e23074404719b02e2d31489badd219fdcdc
Sergey Mosin <sergey@srgdev.com>
2020-10-11 08:17:13 -0400

    docs

--- templates/help.php
+++ templates/help.php
@@ -93,7 +93,7 @@ form{
     <option style="font-size: medium" value="0">In-person meeting</option>
     <option style="font-size: medium" value="1">Online (audio/video)</option>
   </select>
-  <p class="srgdev-appt-hs-p" style="margin-top: 1em;margin-bottom: .75em"><strong id="srgdev-sec_talkTypeChange">Type change email text</strong> - if this field is not empty and has two tokens <code class="srgdev-appt-hs-code_short">{{link_text}}</code> (can contain any text) and <code class="srgdev-appt-hs-code_short">{{new_type}}</code> (MUST be new_type), then this text will be attached to the email and attendees will be able to switch their meeting type simply by clicking a link.</p>
+  <p class="srgdev-appt-hs-p" style="margin-top: 1em;margin-bottom: .75em"><strong id="srgdev-sec_talkTypeChange">Type change email text</strong> - if this field is not empty and has two tokens <code class="srgdev-appt-hs-code_short">{{link_text}}</code> (can contain any text) and <code class="srgdev-appt-hs-code_short">{{new_type}}</code> (MUST be new_type), then this text will be attached to the email and attendees will be able to switch their meeting type simply by clicking the link <code class="srgdev-appt-hs-code_short">{{link_text}}</code>.</p>
   <p class="srgdev-appt-hs-p">For example, this:</p>
   <code class="srgdev-appt-hs-code">
     Click {{here}} to change your appointment type to {{new_type}}.
