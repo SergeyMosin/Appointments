e9040b9ce693d24c35a65da8d38ea4df7656f5f4
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-10-26 15:00:45 +0100

    l10n: Change the triple dot to ellipsis

--- src/components/FormStnSlideBar.vue
+++ src/components/FormStnSlideBar.vue
@@ -79,7 +79,7 @@
                                 v-model="ppsInfo.gdpr"
                                 id="srgdev-appt_pps-gdpr"
                                 type="text"
-                                :placeholder="t('appointments','See Tutorial ...')">
+                                :placeholder="t('appointments','See Tutorial …')">
                         <div style="padding-top: .25em"
                              class="srgdev-appt-sb-chb-cont"><input
                                 v-model="ppsInfo.hidePhone"
