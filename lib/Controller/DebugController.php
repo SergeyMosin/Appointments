02e8c888e55fcddd7cc4d455e84f40cd70a987dc
Sergey Mosin <sergey@srgdev.com>
2021-06-12 14:36:07 -0400

    add version info to `settings dump`

--- lib/Controller/DebugController.php
+++ lib/Controller/DebugController.php
@@ -3,13 +3,14 @@
 
 namespace OCA\Appointments\Controller;
 
+use OC_Util;
 use OCA\Appointments\Backend\BackendUtils;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IConfig;
 use OCP\IRequest;
 
-class DebugController  extends Controller
+class DebugController extends Controller
 {
     private $userId;
     private $config;
@@ -19,45 +20,48 @@ class DebugController  extends Controller
                                 IRequest $request,
                                 $UserId,
                                 IConfig $config,
-                                BackendUtils $utils){
+                                BackendUtils $utils)
+    {
         parent::__construct($AppName, $request);
-
-        $this->userId=$UserId;
-        $this->config=$config;
-        $this->utils=$utils;
+        $this->userId = $UserId;
+        $this->config = $config;
+        $this->utils = $utils;
     }
 
     /**
      * @NoAdminRequired
      */
-    function settingsDump(){
+    function settingsDump()
+    {
 
-        $keys=[
-        BackendUtils::KEY_ORG,
-        BackendUtils::KEY_EML,
-        BackendUtils::KEY_CLS,
-        BackendUtils::KEY_PSN,
-        BackendUtils::KEY_MPS_COL,
-        BackendUtils::KEY_PAGES,
-        BackendUtils::KEY_DIR,
-        BackendUtils::KEY_TALK,
-        BackendUtils::KEY_FORM_INPUTS_JSON,
-        BackendUtils::KEY_FORM_INPUTS_HTML,
+        $keys = [
+            BackendUtils::KEY_ORG,
+            BackendUtils::KEY_EML,
+            BackendUtils::KEY_CLS,
+            BackendUtils::KEY_PSN,
+            BackendUtils::KEY_MPS_COL,
+            BackendUtils::KEY_PAGES,
+            BackendUtils::KEY_DIR,
+            BackendUtils::KEY_TALK,
+            BackendUtils::KEY_FORM_INPUTS_JSON,
+            BackendUtils::KEY_FORM_INPUTS_HTML,
             BackendUtils::KEY_USE_DEF_EMAIL,
             BackendUtils::KEY_EMAIL_FIX,
             BackendUtils::KEY_TMPL_INFO,
             BackendUtils::KEY_TMPL_DATA,
         ];
 
-        $data='<strong>Key</strong>: '.($this->config->getUserValue($this->userId, $this->appName, "cnk")!==""?"Yes":"No")."\n\n";
+        $data ='<strong>Nextcloud Version</strong>: '.OC_Util::getVersionString()."\n"
+            .'<strong>Appointments Version</strong>: '.$this->config->getAppValue($this->appName, 'installed_version', "N/A")."\n"
+            .'<strong>Key</strong>: ' . ($this->config->getUserValue($this->userId, $this->appName, "cnk") !== "" ? "Yes" : "No") . "\n\n";
 
         foreach ($keys as $k) {
-            $data.= '<strong>'.$k.'</strong>: '.var_export($this->utils->getUserSettings(
-                $k, $this->userId),true);
-            $data.="\n\n";
+            $data .= '<strong>' . $k . '</strong>: ' . var_export($this->utils->getUserSettings(
+                    $k, $this->userId), true);
+            $data .= "\n\n";
         }
-        $tr=new TemplateResponse($this->appName,'settings_dump', [],"base");
-        $params['data']=$data;
+        $tr = new TemplateResponse($this->appName, 'settings_dump', [], "base");
+        $params['data'] = $data;
         $tr->setParams($params);
         return $tr;
     }
