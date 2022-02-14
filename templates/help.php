77633c498f1cb56218ab2f58bf637ccec6906e12
Sergey Mosin <sergey@srgdev.com>
2022-02-14 09:41:15 -0500

    fix dark theme styles in "Quick Start Guide" - issue #289

--- templates/help.php
+++ templates/help.php
@@ -53,7 +53,7 @@ form{
     <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emailatt">Email Attendee when the appointment is modified and/or deleted</strong> - Attendees will be notified via email when their <strong>upcoming</strong> appointments are updated or deleted in the calendar app or via some other external mechanism. Only changes to Date/Time, Status or Location will trigger the "Modified" notification.</p>
     <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emailme">Email Me when an appointment is updated</strong> - A notification email will be sent to you when an appointment is booked via the public page or an upcoming appointment is confirmed or canceled via the email links.</p>
     <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emailskipevs">Skip email validation step</strong> - When this option is selected the "<em>... action needed</em>" validation email will NOT be sent to the attendee. Instead the "<em>... Appointment is confirmed</em>" message is going to be sent right away, and the "<em>All done</em>" page is going to be shown when the form is submitted.</p>
-    <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emaildef"><code>useDefaultEmail</code></strong> - Most instance of NC won't have the particular configuration allowing to send emails on behalf of organizers. Therefore, the default email address as per <a style="color: blue; text-decoration: underline" href="https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/email_configuration.html" target="_blank">Mail Settings</a> is used, and your address is added in the "Reply-To:" header field. If your Nextcloud configuration supports sending out emails for individual users, Admins can override the 'useDefaultEmail' directive like so: <code style="background: #eeeeee; padding: 0 .5em">occ config:app:set appointments useDefaultEmail --value no</code></p>
+    <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emaildef"><code>useDefaultEmail</code></strong> - Most instance of NC won't have the particular configuration allowing to send emails on behalf of organizers. Therefore, the default email address as per <a  class="srgdev-appt-hs-link" href="https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/email_configuration.html" target="_blank">Mail Settings</a> is used, and your address is added in the "Reply-To:" header field. If your Nextcloud configuration supports sending out emails for individual users, Admins can override the 'useDefaultEmail' directive like so: <code class="srgdev-appt-hs-code_short">occ config:app:set appointments useDefaultEmail --value no</code></p>
     <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_emailmoretext"><code>Additional Email Text</code></strong>  - this text is appended as paragraph to the end of validation and confirmation emails. Currently only pain text is allowed, HTML will be escaped.</p>
     <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_icsmoretext"><code>Additional ICS file description</code></strong>  - this text (no HTML please) will be appended to the end of the event's "DESCRIPTION" property.</p>
     <h2 class="srgdev-appt-hs-h1" id="srgdev-sec_ts_mode">Time slot mode</h2>
@@ -71,7 +71,7 @@ form{
     </div>
     <p class="srgdev-appt-hs-p-h"><strong>External mode</strong> - Use Nextcloud's Calendar App or any other CalDAV compatible client to add "available" timeslots. Most recurrence rules are supported in this mode. Two calendars are required: a "Source Calendar" to keep track of your availability timeslots and a "Destination Calendar" for booked appointments.</p>
     <div style="margin-left: 2em">
-        <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_sourcecal_nr">Source Calendar (External mode)</strong> - Any event with "Show As" a.k.a. "Time As" a.k.a. "Free/Busy" a.k.a. "Time Transparency" set to "<strong>Free</strong>" (<a style="color: blue; text-decoration: underline" href="https://tools.ietf.org/html/rfc5545#section-3.8.2.7">RFC5545 specs</a> "TRANSP:TRANSPARENT") will be available for booking in the public form. Most recurrence rules are supported. Also see <span style="font-style: italic">Require "Appointment" category.</span></p>
+        <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_sourcecal_nr">Source Calendar (External mode)</strong> - Any event with "Show As" a.k.a. "Time As" a.k.a. "Free/Busy" a.k.a. "Time Transparency" set to "<strong>Free</strong>" (<a class="srgdev-appt-hs-link" href="https://tools.ietf.org/html/rfc5545#section-3.8.2.7">RFC5545 specs</a> "TRANSP:TRANSPARENT") will be available for booking in the public form. Most recurrence rules are supported. Also see <span style="font-style: italic">Require "Appointment" category.</span></p>
         <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_destcal_nr">Destination Calendar (External mode)</strong> - Booked appointments will be placed in here. In addition to booked appointments, any events in this calendar marked as "<strong>Busy</strong>" will prevent conflicting timeslots in the "Source Calendar" from appearing in the public form. Also see <span style="font-style: italic">Require "Appointment" category.</span></p>
         <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_push_rec_nr">Optimize recurrence (External mode)</strong> - If recurrent events are used in the "Source Calendar" the start (DTSTART) date will be pushed forward once in a while in order to improve performance.</p>
         <p class="srgdev-appt-hs-p-h"><strong id="srgdev-sec_require_cat_nr">Require "Appointment" category (External mode)</strong> - When this option is set only events with with "Category" set to "<strong>Appointment</strong>" (in English) will be considered.</p>
@@ -88,7 +88,7 @@ form{
   </code>
   <p class="srgdev-appt-hs-p">will look similar to this:</p>
   <code class="srgdev-appt-hs-code">
-    Please use this link <span style="color: blue;text-decoration: underline">https://my_domain.com/index.php/call/to6d6y4e</span> to contact me.<br>
+    Please use this link <span class="srgdev-appt-hs-link" >https://my_domain.com/index.php/call/to6d6y4e</span> to contact me.<br>
     Your password is: dj984jjr
   </code>
   <p class="srgdev-appt-hs-p" style="margin-top: 1em;margin-bottom: .75em"><strong id="srgdev-sec_talkFF">"Meeting Type" form field</strong> - when this option is enabled, a <code class="srgdev-appt-hs-code_short">&lt;select&gt;</code> drop-down similar to the one bellow will be added to the form. If a visitor selects an <span style="white-space: nowrap">'In-person'</span> meeting, a Talk room for this appointment will NOT be created.</p>
@@ -114,7 +114,7 @@ form{
   </code>
   <p class="srgdev-appt-hs-p">will look similar to this:</p>
   <code class="srgdev-appt-hs-code">
-    Click <span style="color: blue;text-decoration: underline">here</span> to change your appointment type to Online (audio/video).
+    Click <span class="srgdev-appt-hs-link">here</span> to change your appointment type to Online (audio/video).
   </code>
   <p class="srgdev-appt-hs-p">Talk rooms will be created and deleted  automatically when a meeting type changes.</p><br>
   <h2 class="srgdev-appt-hs-h1">iFrame/Embedding</h2>
@@ -135,19 +135,19 @@ if(isset($_GET['some_param_name'])){
 echo '&lt;iframe src = "'.$src.'"&gt;&lt;/iframe&gt;';
 ?&gt;
 ...</code>
-        Nextcloud <strong>occ</strong>: <a style="color: blue; text-decoration: underline" target="_blank" href="https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html">https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html</a><br>
-        Frame Ancestors: <a style="color: blue; text-decoration: underline" target="_blank" href="https://w3c.github.io/webappsec-csp/#directive-frame-ancestors">https://w3c.github.io/webappsec-csp/#directive-frame-ancestors</a><br>
-        Additional information can be found here: <a style="color: blue; text-decoration: underline" target="_blank" href="https://github.com/SergeyMosin/Appointments/issues/191#issuecomment-909210230">https://github.com/SergeyMosin/Appointments/issues/191#issuecomment-909210230</a><br>
-        Some more information is here: <a style="color: blue; text-decoration: underline" target="_blank" href="https://github.com/SergeyMosin/Appointments/issues/268#issue-1067123944">https://github.com/SergeyMosin/Appointments/issues/268#issue-1067123944</a><br>
+        Nextcloud <strong>occ</strong>: <a class="srgdev-appt-hs-link" target="_blank" href="https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html">https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html</a><br>
+        Frame Ancestors: <a class="srgdev-appt-hs-link" target="_blank" href="https://w3c.github.io/webappsec-csp/#directive-frame-ancestors">https://w3c.github.io/webappsec-csp/#directive-frame-ancestors</a><br>
+        Additional information can be found here: <a class="srgdev-appt-hs-link" target="_blank" href="https://github.com/SergeyMosin/Appointments/issues/191#issuecomment-909210230">https://github.com/SergeyMosin/Appointments/issues/191#issuecomment-909210230</a><br>
+        Some more information is here: <a class="srgdev-appt-hs-link" target="_blank" href="https://github.com/SergeyMosin/Appointments/issues/268#issue-1067123944">https://github.com/SergeyMosin/Appointments/issues/268#issue-1067123944</a><br>
     </div>
 
   <h2 id="srgdev-sec_contrib_info" class="srgdev-appt-hs-h1">Contributor Features</h2>
   <div class="srgdev-appt-hs-p">
     Contributor feature can be unlocked by obtaining a contributor key in any of the following ways:
     <ol type="a" style="margin: .5em 0 1em 2em">
-      <li>Contribute any amount to this app development or sponsor a feature over at the <a style="color: blue; text-decoration: underline" target="_blank" href="https://www.srgdev.com/gh-support/nextcloudapps">Funding page</a>.</li>
-      <li>Contribute code via a pull request on <a style="color: blue; text-decoration: underline" target="_blank" href="https://github.com/SergeyMosin/Appointments">GitHub</a>.</li>
-      <li>If you are a member of the Nexcloud team on transifex.com please <a style="color: blue; text-decoration: underline" target="_blank" href="https://www.srgdev.com/contact.html#cnt_ancr">contact me</a> directly.</li>
+      <li>Contribute any amount to this app development or sponsor a feature over at the <a class="srgdev-appt-hs-link" target="_blank" href="https://www.srgdev.com/gh-support/nextcloudapps">Funding page</a>.</li>
+      <li>Contribute code via a pull request on <a class="srgdev-appt-hs-link" target="_blank" href="https://github.com/SergeyMosin/Appointments">GitHub</a>.</li>
+      <li>If you are a member of the Nexcloud team on transifex.com please <a class="srgdev-appt-hs-link" target="_blank" href="https://www.srgdev.com/contact.html#cnt_ancr">contact me</a> directly.</li>
       <li>Contact me if any of the above methods do not work for you.</li>
     </ol>
     Once you receive your key/code you can enter it in the <code class="srgdev-appt-hs-code_short">Settings &gt; Contributor Key</code> section.<br>
